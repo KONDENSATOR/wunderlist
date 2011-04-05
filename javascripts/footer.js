2bcc614d96d8667e93858ac3e9f1ebdaa1441e7e
Robin Enhorn <robin@kondensator.se>
2011-04-05 21:24:39 +0200

    Small changes.

--- javascripts/footer.js
+++ javascripts/footer.js
@@ -8,7 +8,8 @@ footer.keywords_updated = function() {
 
 	for(var keyword in data) {
 		var cloud_data = {
-			tag:data[keyword]
+			tag:data[keyword],
+			id:'1'
 		};
 		
 		var cloud = ich.footer_cloud_template(cloud_data);
@@ -21,7 +22,7 @@ footer.keywords_updated = function() {
 	$(".cloud").each(function(index,object){
 		$(object).callout({
 		    msg:footer.callout_contents_for_object(object),
-			todo_id:$(object).attr('id').replace('cloud_','')
+			todo_id:footer.clean($(object).attr('id'))
 		});
 		
 		$(object).bind('click',function(e){
@@ -33,7 +34,7 @@ footer.keywords_updated = function() {
 }
 
 footer.clean = function (string) {
-	return string.replace('#','').replace(/@/g,'_at_').replace('/','_slash_');
+	return string.replace('cloud_','');
 }
 
 footer.showcallout = function (object) {
